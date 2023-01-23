5d1be7d422569b753a0750cd519912445bd1e61b
Nisvet Mujkić <nisvet.mujkic@outlook.com>
2023-01-23 09:59:26 +0100

    feat: add Console project

---
+++ PowerDiary.Messaging.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PowerDiary.Messaging.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
