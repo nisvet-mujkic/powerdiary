4483b85e2acb6a385384aae8f088208ba2decdf7
Nisvet Mujkić <nisvet.mujkic@outlook.com>
2023-01-25 16:11:36 +0100

    chore: cleaning up

--- PowerDiary.Messaging.Application/Strategies/HourlyAggregation.cs
+++ PowerDiary.Messaging.Application/Strategies/HourlyAggregation.cs
@@ -8,48 +8,50 @@ namespace PowerDiary.Messaging.Application.Strategies
     {
         public void Render(IEnumerable<EventEntry> events)
         {
-            var grouping = events.GroupBy(x => x.Context.OccurredAt.Hour,
+            var groupedByHour = events.GroupBy(x => x.Context.OccurredAt.Hour,
                 (hour, groupedHourly) => new HourGroup(hour, groupedHourly.ToList()));
 
-            foreach (var group in grouping)
+            foreach (var group in groupedByHour)
             {
                 Console.WriteLine(group.Hour);
 
-                var other = group.Events.GroupBy(x => x.Context.EventType,
+                var groupedByEventType = group.Events.GroupBy(x => x.Context.EventType,
                     (eventType, events) => new EventGroup(eventType, events.ToList()));
 
-                foreach (var item in other)
+                foreach (var item in groupedByEventType)
                 {
-                    Console.WriteLine($"\t{Message(item)}");
+                    Console.WriteLine($"\t{BuildMessage(item)}");
                 }
             }
         }
 
-        private static string Message(EventGroup group)
+        private static string BuildMessage(EventGroup group)
         {
             var eventCount = group.Events.Count;
 
-            var xxx = string.Empty;
-
-            if (group.EventType == Constants.EventType.HighFive)
-            {
-                var x = group.Events.GroupBy(x => x.Context.Participant, (from, to) => new HighFivesGroup(from.Name, to.Select(x => x.Context.OtherParticipant).ToList()));
-
-                var c = x.Count();
-                xxx = $"{c} {PluralizeWord("participant", "participants", c)} high-fived {x.Sum(x => x.To.Count)} {PluralizeWord("participant", "participants", x.Sum(x => x.To.Count))}";
-            }
-
             return group.EventType switch
             {
-                Constants.EventType.EnterTheRoom => $"{eventCount} {PluralizeWord("participant", "participants", eventCount)} entered",
-                Constants.EventType.LeaveTheRoom => $"{eventCount} {PluralizeWord("participant", "participants", eventCount)} left",
-                Constants.EventType.Comment => $"{eventCount} {PluralizeWord("comment", "comments", eventCount)}",
-                Constants.EventType.HighFive => xxx
+                Constants.EventType.EnterTheRoom => $"{eventCount} {Pluralize("participant", "participants", eventCount)} entered",
+                Constants.EventType.LeaveTheRoom => $"{eventCount} {Pluralize("participant", "participants", eventCount)} left",
+                Constants.EventType.Comment => $"{eventCount} {Pluralize("comment", "comments", eventCount)}",
+                Constants.EventType.HighFive => BuildMessageForHighFiveEvent(group),
             };
         }
 
-        // create class
-        private static string PluralizeWord(string singular, string plural, int count) =>
+        private static string BuildMessageForHighFiveEvent(EventGroup eventGroup)
+        {
+            var events = GetHiveFiveEvents(eventGroup);
+            var count = events.Count();
+            var highFivesCount = events.Sum(x => x.To.Count);
+
+            return $"{count} {Pluralize("participant", "participants", count)} high-fived {highFivesCount} {Pluralize("participant", "participants", highFivesCount)}";
+        }
+
+        private static IEnumerable<HighFivesGroup> GetHiveFiveEvents(EventGroup eventGroup) =>
+            eventGroup.Events.GroupBy(x => x.Context.Participant, (from, to) =>
+                    new HighFivesGroup(from.Name, to.Select(x => x.Context.OtherParticipant).ToList()));
+
+        private static string Pluralize(string singular, string plural, int count) =>
             count > 1 ? plural : singular;
     }
 
@@ -57,5 +59,5 @@ namespace PowerDiary.Messaging.Application.Strategies
 
     public record EventGroup(string EventType, IReadOnlyCollection<EventEntry> Events);
 
-    public record HighFivesGroup(string from, IReadOnlyCollection<Participant> To);
+    public record HighFivesGroup(string From, IReadOnlyCollection<Participant> To);
 }
